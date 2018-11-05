f5a7b61e0a77584e46c12b670128d4f076da83db
Kovacs Reka Katalin <kovacs.reka.katalin@gmail.com>
2018-11-05 17:45:28 +0100

    create two classes, comment is include

---
+++ src/FilePartReader.java
@@ -0,0 +1,27 @@
+package src;
+
+public class FilePartReader {
+}
+
+/*
+It has one constructor :
+    it sets the class' instance variables to some invalid default value
+It has three instance methods:
+    public void setup ()
+        it's parameters are:
+            filePath as a String
+            fromLine as an Integer
+            toLine as an Integer
+        it throws an IllegalArgumentException :
+            if toLine is smaller than fromLine
+            if fromLine is smaller than 1
+    public String read ()
+        opens the file on filePath , and gives back it's content as a String
+        it doesn't catch the exception being raised, if the file isn't present on filePath ,
+        so actually the method throws the exception it received
+    public String readLines ():
+        reads the file with read ()
+        it gives back every line from it's content between fromLine and toLine (both of them are included),
+        and returns these lines as a String. Take care because if fromLine is 1, it means the very first row in the file.
+        Also, if fromLine is 1 and toLine is 1 also, we will read only the very first line.
+ */
---
+++ src/FileWordAnalyzer.java
@@ -0,0 +1,21 @@
+package src;
+
+public class FileWordAnalyzer {
+
+}
+
+/*
+It has one constructor :
+    it's parameter is a FilePartReader object
+It has three instance methods:
+    public ArrayList wordsByABC ():
+        calls FilePartReader.readLines ()
+        returns the words ordered by alphabetically as an ArrayList
+    public ArrayList wordsContainingSubString (String subString ):
+        calls FilePartReader.readLines ()
+        returns the words which contains the subString
+    public ArrayList wordsArePalindrome ():
+        calls FilePartReader.readLines ()
+        returns the words from the String which are palindrome
+
+ */
