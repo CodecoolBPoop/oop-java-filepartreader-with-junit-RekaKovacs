8f7e76238e99ea622a8010078746ddc48b8ae06c
Kovacs Reka Katalin <kovacs.reka.katalin@gmail.com>
2018-11-06 08:37:23 +0100

    readlines method is completed, FilePartReader classes is completed.

--- src/com/codecool/main/java/FilePartReader.java
+++ src/com/codecool/main/java/FilePartReader.java
@@ -3,18 +3,19 @@ package src.com.codecool.main.java;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class FilePartReader {
-    String filePath;
-    Integer fromLine;
-    Integer toLine;
+    private String filePath;
+    private Integer fromLine;
+    private Integer toLine;
 
     /*
     It has one constructor :
     it sets the class' instance variables to some invalid default value
      */
     public FilePartReader() {
-        filePath = "blablabla";
+        filePath = "src/com/codecool/main/resources/dickensTaleOfTwoCities.txt";
         fromLine = 3;
         toLine = 2;
     }
@@ -29,7 +30,7 @@ public class FilePartReader {
             if toLine is smaller than fromLine
             if fromLine is smaller than 1
      */
-    public void setup () {
+    public void setup (String filePath, Integer fromLine, Integer toLine) {
 
         if (toLine < fromLine || fromLine < 1) {
             throw new IllegalArgumentException();
@@ -37,6 +38,7 @@ public class FilePartReader {
     }
 
     /*
+    public String read ()
     opens the file on filePath , and gives back it's content as a String
         it doesn't catch the exception being raised, if the file isn't present on filePath ,
         so actually the method throws the exception it received
@@ -45,19 +47,33 @@ public class FilePartReader {
         BufferedReader in = null;
         String s;
         StringBuilder sb = new StringBuilder();
-        in = new BufferedReader(new FileReader("src/com/codecool/main/resources/dickensTaleOfTwoCities.txt"));
+        in = new BufferedReader(new FileReader(filePath));
         while ((s = in.readLine()) != null)
                 sb.append(s + "\n");
         return sb.toString();
     }
-}
 
-/*
-    public String read ()
-
-    public String readLines ():
-        reads the file with read ()
+    /*
+    reads the file with read ()
         it gives back every line from it's content between fromLine and toLine (both of them are included),
         and returns these lines as a String. Take care because if fromLine is 1, it means the very first row in the file.
         Also, if fromLine is 1 and toLine is 1 also, we will read only the very first line.
- */
+     */
+    public String readLines () throws IOException {
+
+        String allLines = this.read();
+        Scanner scanner = new Scanner(allLines);
+        StringBuilder sb = new StringBuilder();
+        int counter = 0;
+
+        while (scanner.hasNextLine()) {
+            counter++;
+            if (counter >= fromLine && counter <= toLine) {
+                String line = scanner.nextLine();
+                sb.append(line);
+            }
+        }
+        scanner.close();
+        return sb.toString();
+    }
+}
